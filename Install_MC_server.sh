51df6213d54ce9e472b6ddea634d9338a929208b
Zac-Sim <98369322+Zac-Sim@users.noreply.github.com>
2022-02-01 13:36:28 +1100

    Create Install_MC_server.sh

---
+++ Install_MC_server.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+su
+sudo add-apt-repository ppa:linuxuprising/java -y
+sudo apt update
+sudo apt-get install oracle-java17-installer oracle-java17-set-default
+
+sudo apt install screen
+mkdir minecraft
+cd minecraft
